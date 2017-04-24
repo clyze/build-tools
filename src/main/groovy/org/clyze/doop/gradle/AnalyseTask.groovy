86f672e7e850da61a76fde716940258943a90b93
George Fourtounis <gfour@di.uoa.gr>
2017-04-24 13:54:48 +0300

    Fix landing page URL after 'analyse'.

--- src/main/groovy/org/clyze/doop/gradle/AnalyseTask.groovy
+++ src/main/groovy/org/clyze/doop/gradle/AnalyseTask.groovy
@@ -186,7 +186,7 @@ class AnalyseTask extends DefaultTask {
     }
 
     private static String createAnalysisPageURL(String host, int port, String postedId, String token = null) {
-        return "http://$host:$port/clue/main.html" + (token ? "?t=$token" : "") + "#show/$postedId"
+        return "http://$host:$port/clue/main.html" + (token ? "?t=$token" : "") + "#/analyses/$postedId"
     }
 
     private static void openBrowser(String url) {
