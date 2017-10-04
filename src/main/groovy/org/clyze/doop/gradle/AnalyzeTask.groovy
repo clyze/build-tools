3b3b8a744a99b61747ff4e0cc9edc38ff05a830c
George Fourtounis <gfour@di.uoa.gr>
2017-10-04 12:51:41 +0300

    Move POST caching logic to clue-client.

--- src/main/groovy/org/clyze/doop/gradle/AnalyzeTask.groovy
+++ src/main/groovy/org/clyze/doop/gradle/AnalyzeTask.groovy
@@ -1,15 +1,9 @@
 package org.clyze.doop.gradle
 
-import groovy.json.JsonSlurper
-import org.apache.http.HttpEntity
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.client.methods.HttpUriRequest
 import org.clyze.client.web.Helper
-import org.clyze.client.web.RestCommandBase
+import org.clyze.client.web.PostState
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
-
-import java.awt.*
 
 class AnalyzeTask extends DefaultTask {
 
@@ -32,149 +26,8 @@ class AnalyzeTask extends DefaultTask {
             hprof = new File(doop.hprof)
         }
 
-        // Optionally save the data to be posted to the server, so
-        // that they can be reused in the future (see main() below).
-        if (doop.cachePost) {
-            String tmpDir = java.nio.file.Files.createTempDirectory("").toString()
-            println "Caching output to ${tmpDir}"
-
-            def copyToTmp = { File f ->
-                File newFile = new File("${tmpDir}/${f.name}")
-                newFile << f.bytes
-                newFile
-            }
-
-            // Replace all files with their copies.
-            doop.options.inputs = doop.options.inputs.collect { copyToTmp(it) }
-            if (sources          != null) { sources          = copyToTmp(sources)          }
-            if (jcPluginMetadata != null) { jcPluginMetadata = copyToTmp(jcPluginMetadata) }
-            if (hprof            != null) { hprof            = copyToTmp(sources)          }
-
-            // Save remaining information.
-            String tmpFileName = "${tmpDir}/analysis.json"
-            println "Writing ${tmpFileName}..."
-            new File(tmpFileName) << doop.toCacheJson(sources, jcPluginMetadata, hprof)
-            println "Analysis submission data saved in ${tmpDir}"
-        }
-
-        connectPostAndStartAnalysis(doop, sources, jcPluginMetadata, hprof)
-    }
-
-    public static void connectPostAndStartAnalysis(DoopExtension doop, File sources, File jcPluginMetadata, File hprof) {
-        println "Connecting to server at ${doop.host}:${doop.port}"
-        String token = createLoginCommand(doop).execute(doop.host, doop.port)
-
-        println "Submitting ${doop.projectName} version ${doop.projectVersion} for ${doop.options.analysis} analysis"
-        postAndStartAnalysis(doop, sources, jcPluginMetadata, hprof, token)
-    }
-
-    private static void postAndStartAnalysis(DoopExtension doop, File sources, File jcPluginMetadata, File hprof, String token) {
-
-        def authenticator = {String h, int p, HttpUriRequest request ->
-            //send the token with the request
-            request.addHeader(RestCommandBase.HEADER_TOKEN, token)
-        }
-
-        String autoLoginToken = null
-
-        RestCommandBase<String> postAnalysis = createPostCommand(doop, sources, jcPluginMetadata, hprof, authenticator)
-        String postedId = postAnalysis.execute(doop.host, doop.port)
-        println "The analysis has been submitted successfully: $postedId."
-
-        RestCommandBase<String> createAutoLoginToken = createAutoLoginTokenCommand(authenticator)
-        try {
-            autoLoginToken = createAutoLoginToken.execute(doop.host, doop.port)
-
-        }
-        catch(Exception e) {
-            println "Autologin failed: ${e.getMessage()}"
-        }
-
-        String analysisPageURL = createAnalysisPageURL(doop.host, doop.port, postedId, autoLoginToken)
-
-        RestCommandBase<Void> start = createStartCommand(postedId, authenticator)
-        start.onSuccess = { HttpEntity ent ->
-
-            if (autoLoginToken) {
-                println "Sit back and relax while we analyze your code..."
-                try {
-                    openBrowser(analysisPageURL)
-                } catch(Exception e) {
-                    println "Analysis has been posted to the server, please visit $analysisPageURL"
-                }
-            }
-            else {
-                println "Visit $analysisPageURL"
-            }
-        }
-        start.execute(doop.host, doop.port)
-    }
-
-    private static RestCommandBase<String> createPostCommand(DoopExtension doop, 
-                                                             File sources, 
-                                                             File jcPluginMetadata,
-                                                             File hprof, 
-                                                             Closure authenticator) {
-
-        RestCommandBase<String> command = Helper.createPostDoopAnalysisCommand(
-            doop.orgName,
-            doop.projectName,
-            doop.projectVersion,
-            null, //rating
-            null, //ratingCount
-            sources,
-            jcPluginMetadata,
-            hprof,
-            doop.options
-        )
-        command.authenticator = authenticator
-        return command
-    }
-
-    private static RestCommandBase<String> createAutoLoginTokenCommand(Closure authenticator) {
-        return new RestCommandBase<String>(
-            endPoint: "token",
-            requestBuilder:  { String url ->
-                return new HttpPost(url)
-            },
-            onSuccess: { HttpEntity entity ->
-                def json = new JsonSlurper().parse(entity.getContent(), "UTF-8")
-                return json.token
-            },
-            authenticator: authenticator
-        )
-    }
-
-    private static RestCommandBase<Void> createStartCommand(String id, Closure authenticator) {
-        RestCommandBase<Void> command = Helper.createStartCommand(id)
-        command.authenticator = authenticator
-        return command
-    }
-
-    private static RestCommandBase<String> createLoginCommand(DoopExtension doop) {
-        return Helper.createLoginCommand(doop.username, doop.password)        
-    }
-
-    private static String createAnalysisPageURL(String host, int port, String postedId, String token = null) {
-        return "http://$host:$port/clue/" + (token ? "?t=$token" : "") + "#/analyses/$postedId"
-    }
-
-    private static void openBrowser(String url) {
-        File html = File.createTempFile("_doop", ".html")
-        html.withWriter('UTF-8') { w ->
-            w.write """\
-                    <html>
-                        <head>
-                            <script>
-                                document.location="$url"
-                            </script>
-                        </head>
-                        <body>
-                        </body>
-                    </html>
-                    """.stripIndent()
-        }
-        Desktop.getDesktop().browse(html.toURI())
+        PostState ps = doop.newPostState(sources, jcPluginMetadata, hprof)
+        Helper.postAndStartAnalysis(ps, doop.cachePost)
     }
 
     // Entry point to call when replaying a previously posted
@@ -189,7 +42,7 @@ class AnalyzeTask extends DefaultTask {
 
         String path = args[0]
         println "Reading state from ${path}..."
-        def deserialized = DoopExtension.fromCacheJson(path)
+        def deserialized = PostState.fromJson(path)
         File sources = deserialized["sources"]
         File hprof = deserialized["hprof"]
         File jcPluginMetadata = deserialized["jcPluginMetadata"]
--- src/main/groovy/org/clyze/doop/gradle/DoopExtension.groovy
+++ src/main/groovy/org/clyze/doop/gradle/DoopExtension.groovy
@@ -1,7 +1,6 @@
 package org.clyze.doop.gradle
 
-import groovy.json.JsonBuilder
-import groovy.json.JsonSlurper
+import org.clyze.client.web.PostState
 import org.gradle.util.ConfigureUtil
 
 class DoopExtension {
@@ -33,49 +32,9 @@ class DoopExtension {
         return (host != null) && (port != 0) && (username != null) && (password != null) && (subprojectName != null) && (buildType != null)
     }
 
-    // Serialize information needed to replay analysis posting.
-    public String toCacheJson(File sources, File jcPluginMetadata, File hprof) {
-        String optionsJson = new JsonBuilder(options).toPrettyString()
-        String sourcesName = sources == null? "null" : "\"${sources.name}\""
-        String jcPluginMetadataName = jcPluginMetadata == null? "null" : "\"${jcPluginMetadata.name}\""
-        String hprofName = hprof == null? "null" : "\"${hprof.name}\""
-        return "{ \"host\" : \"${host}\",\n" +
-               "  \"port\" : \"${port}\",\n" +
-               "  \"username\" : \"${username}\",\n" +
-               "  \"password\" : \"${password}\",\n" +
-               "  \"orgName\" : \"${orgName}\",\n" +
-               "  \"projectName\" : \"${projectName}\",\n" +
-               "  \"projectVersion\" : \"${projectVersion}\",\n" +
-               "  \"options\" : ${optionsJson},\n" +
-               "  \"sourcesName\" : ${sourcesName},\n" +
-               "  \"jcPluginMetadataName\" : ${jcPluginMetadataName},\n" +
-               "  \"hprofName\" : ${hprofName}\n" +
-               "}"
-    }
-
-    // Restore a DoopExtension object that contains enough information
-    // to replay the posting of an analysis to the server.
-    public static def fromCacheJson(String dir) {
-        File jsonFile = new File("${dir}/analysis.json")
-        Object obj = (new JsonSlurper()).parseText(jsonFile.text)
-        DoopExtension doop = new DoopExtension()
-        doop.host = obj.host
-        doop.port = obj.port.toInteger()
-        doop.username = obj.username
-        doop.password = obj.password
-        doop.orgName = obj.orgName
-        doop.projectName = obj.projectName
-        doop.projectVersion = obj.projectVersion
-        doop.options = obj.options
-
-        // Fix the paths of inputs to point to the given directory.
-        def dirFile = { String n -> n == null ? null : new File("${dir}/${n}") }
-        doop.options.inputs = doop.options.inputs.collect { dirFile(it.name) }
-
-        return [ doop             : (DoopExtension)doop,
-                 sources          : dirFile(obj.sourcesName),
-                 jcPluginMetadata : dirFile(obj.jcPluginMetadataName),
-                 hprof            : dirFile(obj.hprofName)
-               ]
+    public PostState newPostState(File sources, File jcPluginMetadata, File hprof) {
+        return new PostState(host, port, username, password, orgName,
+                             projectName, projectVersion, options, sources,
+                             jcPluginMetadata, hprof)
     }
 }
