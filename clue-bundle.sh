194b7d05f746c741aa92095354bfb4143d7862c6
George Fourtounis <gfour@di.uoa.gr>
2019-10-31 19:09:48 +0200

    Update helper script.

--- clue-bundle.sh
+++ clue-bundle.sh
@@ -1,21 +1,74 @@
 #!/usr/bin/env bash
 
-if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ]
-then
-    echo "Usage: clue-bundle.sh BUNDLE_DIR ASSEMBLE_TASK APK"
+function usage() {
+    echo "Analyze bundle:"
+    echo "  clue-bundle.sh analyze"
+    echo "Pack bundle:"
+    echo "  clue-bundle.sh pack BUNDLE_DIR APK"
     echo
     echo "Generates the appropriate files to be posted to the Web UI."
     echo "This script should be run in the application module directory."
     echo
     echo "  BUNDLE_DIR      example: bundle-app"
-    echo "  ASSEMBLE_TASK   example: assembleDebug"
     echo "  APK             example: build/outputs/apk/app-debug.apk"
+    echo
     exit
-fi
+}
 
-BUNDLE_DIR="$1"
-ASSEMBLE_TASK="$2"
-APK="$3"
+# Run actions that are compatible with default build (assumed
+# "minifyEnabled = true").
+function runDefaultBuildActions() {
+    time ${GRADLE} --info clean configurations
+}
+
+# Build with minifyEnabled = false.
+function runCustomBuildActions() {
+    BUILD2=build-minifyDisabled.gradle
+    grep -vF minifyEnabled build.gradle | grep -vF shrinkResources > ${BUILD2}
+    # Temporarily swap build.gradle with custom one. We cannot use a build
+    # script with a different name, as that may clash with existing settings files.
+    mv build.gradle build.gradle.backup
+    mv ${BUILD2} build.gradle
+    time ${GRADLE} --info clean sourcesJar jcpluginZip codeApk
+    mv build.gradle.backup build.gradle
+}
+
+function addCustomBuildFilesToBundle() {
+    cp ${APK} ${BUNDLE_DIR}
+}
+
+function prepareBundleDir() {
+    mkdir -p ${BUNDLE_DIR}
+    rm -f ${BUNDLE_DIR}/*.apk
+    rm -f ${BUNDLE_DIR}/*.zip
+    rm -f ${BUNDLE_DIR}/*.jar
+}
+
+function runBuildActions() {
+    runDefaultBuildActions
+    runCustomBuildActions
+}
+
+function createBundleArchive() {
+    cp ${LOCAL_BUNDLE_DIR}/metadata.zip ${BUNDLE_DIR}
+    cp ${LOCAL_BUNDLE_DIR}/configurations.zip ${BUNDLE_DIR}
+    cp ${LOCAL_BUNDLE_DIR}/sources.jar ${BUNDLE_DIR}
+
+    BUNDLE_FILE=bundle.tar.gz
+    rm -f ${BUNDLE_FILE}
+    tar -czf ${BUNDLE_FILE} ${BUNDLE_DIR}
+    echo "Output written to file: ${BUNDLE_FILE}"
+}
+
+if [ "$1" != "analyze" ] && [ "$1" != "pack" ]; then
+    usage
+elif [ "$1" == "analyze" ] && [ "$2" != "" ]; then
+    usage
+elif [ "$1" == "pack" ]; then
+     if [ "$2" == "" ] || [ "$3" == "" ]; then
+         usage
+     fi
+fi
 
 # Autodetect gradlew wrapper.
 if [ "${GRADLE}" == "" ]; then
@@ -29,31 +82,16 @@ if [ "${GRADLE}" == "" ]; then
     fi
 fi
 
-mkdir -p ${BUNDLE_DIR}
-rm -f ${BUNDLE_DIR}/*.apk
-rm -f ${BUNDLE_DIR}/*.zip
-rm -f ${BUNDLE_DIR}/*.jar
+LOCAL_BUNDLE_DIR=".clue-bundle"
 
-# Build with minifyEnabled = true.
-time ${GRADLE} --info clean ${ASSEMBLE_TASK} sourcesJar jcpluginZip configurations
-
-cp build/scavenge/metadata.zip ${BUNDLE_DIR}
-cp build/scavenge/configurations.zip ${BUNDLE_DIR}
-cp build/libs/sources.jar ${BUNDLE_DIR}
-
-# Build with minifyEnabled = false.
-BUILD2=build-minifyDisabled.gradle
-grep -vF minifyEnabled build.gradle | grep -vF shrinkResources > ${BUILD2}
-# Temporarily swap build.gradle with custom one. We cannot use a build
-# script with a different name, as that may clash with existing settings files.
-mv build.gradle build.gradle.backup
-mv ${BUILD2} build.gradle
-time ${GRADLE} clean codeApk
-mv build.gradle.backup build.gradle
-
-cp ${APK} ${BUNDLE_DIR}
-
-BUNDLE_FILE=bundle.tar.gz
-rm -f ${BUNDLE_FILE}
-tar -czf ${BUNDLE_FILE} ${BUNDLE_DIR}
-echo "Output written to file: ${BUNDLE_FILE}"
+if [ "$1" == "pack" ]; then
+    BUNDLE_DIR="$2"
+    APK="$3"
+    prepareBundleDir
+    runBuildActions
+    addCustomBuildFilesToBundle
+    createBundleArchive
+elif [ "$1" == "analyze" ]; then
+    runBuildActions
+    ${GRADLE} analyze
+fi
