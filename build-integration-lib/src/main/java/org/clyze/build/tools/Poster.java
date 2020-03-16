348321509cb28d11e607dc918e3e07b98de677e3
George Fourtounis <gfour@di.uoa.gr>
2020-03-16 10:59:47 +0200

    Handle connection errors.

--- build-integration-lib/src/main/java/org/clyze/build/tools/Poster.java
+++ build-integration-lib/src/main/java/org/clyze/build/tools/Poster.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.clyze.client.web.Helper;
 import org.clyze.client.web.PostState;
 import org.clyze.client.web.api.Remote;
+import org.apache.http.conn.HttpHostConnectException;
 
 /**
  * This is a thin interface over the clue-client functionality that
@@ -27,6 +28,7 @@ public class Poster {
     }
 
     public void post(PostState ps, List<Message> messages) {
+        // Optional: save state that will be uploaded.
         if (cachePost) {
             try {
                 File tmpDir = Files.createTempDirectory("").toFile();
@@ -37,29 +39,33 @@ public class Poster {
             }
         }
 
-        // Check if the server can receive Android bundles.
-        if (android) {
-            Map<String, String> diag = Remote.at(options.host, options.port).diagnose();
-            String androidSDK_OK = diag.get("ANDROID_SDK_OK");
-            if ((androidSDK_OK != null) && (androidSDK_OK.equals("false"))) {
-                Message.print(messages, "ERROR: Cannot post bundle: Android SDK setup missing.");
-                return;
-            }
-        }
-
+        // Optional: save post request options.
         if (metadataDir != null) {
             File metadataFile = new File(metadataDir, Conventions.POST_METADATA);
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(metadataFile))) {
-                Message.print(messages, "Saving options in: " + metadataFile.getCanonicalPath());
+                Message.debug(messages, "Saving options in: " + metadataFile.getCanonicalPath());
                 writer.write(ps.toJSON());
             } catch (IOException ex) {
                 Message.warn(messages, "WARNING: cannot save metadata: " + ex.getMessage());
             }
         }
 
-        if (!options.dry) {
-            Helper.doPost(options.host, options.port, options.username,
-                          options.password, options.project, options.profile, ps);
+        try {
+            // Check if the server can receive Android bundles.
+            if (android) {
+                Map<String, String> diag = Remote.at(options.host, options.port).diagnose();
+                String androidSDK_OK = diag.get("ANDROID_SDK_OK");
+                if ((androidSDK_OK != null) && (androidSDK_OK.equals("false"))) {
+                    Message.print(messages, "ERROR: Cannot post bundle: Android SDK setup missing.");
+                    return;
+                }
+            }
+            if (!options.dry)
+                Helper.doPost(options.host, options.port, options.username,
+                              options.password, options.project, options.profile, ps);
+        } catch (HttpHostConnectException ex) {
+            Message.print(messages, "ERROR: could not post bundle, is the server running?");
+            return;
         }
     }
 
