ab3d9ac6473cd28cc139a6e784488bca0e9d3531
George Fourtounis <gfour@di.uoa.gr>
2020-09-21 13:00:02 +0300

    Skip unused parameter.

--- build-integration-lib/src/main/java/com/clyze/build/tools/Poster.java
+++ build-integration-lib/src/main/java/com/clyze/build/tools/Poster.java
@@ -55,7 +55,7 @@ public class Poster {
         if (options.dry)
             System.err.println("WARNING: automated repackaging ignores dry option.");
         Helper.repackageBuildForCI(options.host, options.port, options.username,
-                                   options.password, options.project, options.platform, null, ps, handler);
+                                   options.password, options.project, options.platform, ps, handler);
     }
 
 }
